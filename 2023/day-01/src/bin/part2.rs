e950ba97cfa8fdb413e51526a7823e481aff1e5c
phon <hassanjaved186@gmail.com>
2023-12-08 08:00:16 +0000

    Add day 1

--- 2023/day-01/src/bin/part2.rs
+++ 2023/day-01/src/bin/part2.rs
@@ -13,9 +13,22 @@ fn main() {
         Err(error) => panic!("Error: {}", error),
     };
 
+
+
     let output = input
         .lines()
         .map(|line| {
+            let line = line
+                .replace("one", "o1e")
+                .replace("two", "t2o")
+                .replace("three", "t3e")
+                .replace("four", "f4r")
+                .replace("five", "f5e")
+                .replace("six", "s6x")
+                .replace("seven", "s7n")
+                .replace("eight", "e8t")
+                .replace("nine", "n9e");
+
             let mut iter =
                 line.chars().filter_map(|char| {
                     char.to_digit(10)
